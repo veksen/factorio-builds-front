27bec44a2d910401c10e0cd7938a46a6196a8f63
Jean-Philippe Sirois <jp@veksen.net>
2017-11-17 01:30:32 -0500

    chore: clean up boilerplate stuff

--- app/containers/Filter/tests/selectors.test.js
+++ app/containers/Filter/tests/selectors.test.js
@@ -1,10 +1,20 @@
-// import { fromJS } from 'immutable';
-// import { makeSelectFilterDomain } from '../selectors';
+import { fromJS } from 'immutable';
+import { makeSelectFilterOnlyBluePrints } from '../selectors';
 
 // const selector = makeSelectFilterDomain();
 
 describe('makeSelectFilterDomain', () => {
   it('Expect to have unit tests specified', () => {
     expect(true).toEqual(false);
+  });
+});
+
+describe('makeSelectFilterOnlyBluePrints', () => {
+  it('should select the blueprint filter', () => {
+    const globalState = fromJS(true);
+    const mockedState = fromJS({
+      onlyWithBlueprints: globalState,
+    });
+    expect(makeSelectFilterOnlyBluePrints(mockedState)).toEqual(globalState);
   });
 });
