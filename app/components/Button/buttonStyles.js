b64ef52042ae35cc1493f0aaaae96c9de49d008c
Jean-Philippe Sirois <jp@veksen.net>
2017-11-25 23:49:31 -0500

    refact: move the Header button styles to the Button component

--- app/components/Button/buttonStyles.js
+++ app/components/Button/buttonStyles.js
@@ -1,23 +1,35 @@
 import { css } from 'styled-components';
 
 const buttonStyles = css`
-  display: inline-block;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   box-sizing: border-box;
-  padding: 0.25em 2em;
+  padding: 0 14px;
   text-decoration: none;
-  border-radius: 4px;
+  border-radius: 5px;
   -webkit-font-smoothing: antialiased;
   -webkit-touch-callout: none;
   user-select: none;
   cursor: pointer;
   outline: 0;
   font-size: 17px;
-  border: 2px solid #41addd;
-  color: #41addd;
+  border: 0;
+  color: #fff;
+  background-color: #543B7A;
+  background-image: linear-gradient(#7950B9 0%, #543B7A 100%);
+  height: 38px;
+  transition: all .2s ease;
+
+  &:hover,
+  &:focus {
+    background-position: 0 -100px;
+    transform: translateY(-1px);
+    box-shadow: 0 4px 12px rgba(0, 0, 0, 0.2), inset 0 0 12px #543B7A;
+  }
 
   &:active {
-    background: #41addd;
-    color: #fff;
+    opacity: 0.85;
   }
 `;
 
