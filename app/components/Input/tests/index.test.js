615fbd6790b7590c9ff085a419d357b8121bc849
Jean-Philippe Sirois <jp@veksen.net>
2017-12-10 21:00:58 -0500

    tests: fix issues making the test suite fail to run

--- app/components/Input/tests/index.test.js
+++ app/components/Input/tests/index.test.js
@@ -1,10 +1,27 @@
-// import React from 'react';
-// import { shallow } from 'enzyme';
+import React from 'react';
+import { shallow } from 'enzyme';
 
-// import Input from '../index';
+import Input from '../index';
 
 describe('<Input />', () => {
-  it('Expect to have unit tests specified', () => {
-    expect(true).toEqual(false);
+  it('should render an <input> tag', () => {
+    const renderedComponent = shallow(<Input />);
+    expect(renderedComponent.type()).toEqual('input');
+  });
+
+  it('should have a className attribute', () => {
+    const renderedComponent = shallow(<Input />);
+    expect(renderedComponent.prop('className')).toBeDefined();
+  });
+
+  it('should adopt a valid attribute', () => {
+    const id = 'test';
+    const renderedComponent = shallow(<Input id={id} />);
+    expect(renderedComponent.prop('id')).toEqual(id);
+  });
+
+  it('should not adopt an invalid attribute', () => {
+    const renderedComponent = shallow(<Input attribute={'test'} />);
+    expect(renderedComponent.prop('attribute')).toBeUndefined();
   });
 });
