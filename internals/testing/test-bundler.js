c6dd957dc3c995792736c0c7e696d7958420326d
Jean-Philippe Sirois <jp@veksen.net>
2018-01-11 18:05:16 -0500

    chore(deps): upgrade React to 16.2.0

--- internals/testing/test-bundler.js
+++ internals/testing/test-bundler.js
@@ -1,3 +1,11 @@
 // needed for regenerator-runtime
 // (ES7 generator support is required by redux-saga)
 import 'babel-polyfill';
+
+// Polyfill for requestAnimationFrame
+import 'raf/polyfill';
+
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
