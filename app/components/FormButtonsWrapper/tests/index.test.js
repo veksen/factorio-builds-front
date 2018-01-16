7146bbe70413cc9aa29fceb111e5d44fd338aa73
Jean-Philippe Sirois <jp@veksen.net>
2018-01-16 03:18:04 -0500

    tests: add basic tests for FormButtonsWrapper

--- app/components/FormButtonsWrapper/tests/index.test.js
+++ app/components/FormButtonsWrapper/tests/index.test.js
@@ -1,10 +1,26 @@
-// import React from 'react';
-// import { shallow } from 'enzyme';
+import React from 'react';
+import { shallow } from 'enzyme';
 
-// import FormButtonsWrapper from '../index';
+import FormButtonsWrapper from '../index';
+
+const childrenOne = (<button>Test One</button>);
+const childrenTwo = (<button>Test Two</button>);
+const renderComponent = () => shallow(
+  <FormButtonsWrapper>
+    {childrenOne}
+    {childrenTwo}
+  </FormButtonsWrapper>
+).dive();
 
 describe('<FormButtonsWrapper />', () => {
-  it.skip('Expect to have unit tests specified', () => {
-    expect(true).toEqual(false);
+  it('should be a div', () => {
+    const renderedComponent = renderComponent();
+    expect(renderedComponent.type()).toEqual('div');
+  });
+
+  it('should render children', () => {
+    const renderedComponent = renderComponent();
+    expect(renderedComponent.contains(childrenOne)).toEqual(true);
+    expect(renderedComponent.contains(childrenTwo)).toEqual(true);
   });
 });
