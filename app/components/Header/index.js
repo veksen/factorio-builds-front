6756810d7a7aff8fcb2ef374c84396f1e29f4f5a
Jean-Philippe Sirois <jp@veksen.net>
2018-01-16 03:17:26 -0500

    tests(Header): wip on first tests

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -14,7 +14,7 @@ import NavBar from './NavBar';
 import HeaderLink from './HeaderLink';
 import messages from './messages';
 
-class Header extends React.Component { // eslint-disable-line react/prefer-stateless-function
+export class Header extends React.Component { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
       <HeaderWrapper>
--- app/components/Header/tests/index.test.js
+++ app/components/Header/tests/index.test.js
@@ -1,13 +1,55 @@
 import React from 'react';
+// import { Provider } from 'react-redux';
+// import { browserHistory } from 'react-router-dom';
 import { shallow } from 'enzyme';
 
-import Header from '../index';
+// import LanguageProvider from 'containers/LanguageProvider';
+
+// import configureStore from '../../../configureStore';
+// import { translationMessages } from '../../../i18n';
+
+import { Header } from '../index';
+import Title from '../Title';
+import HeaderLink from '../HeaderLink';
 
 describe('<Header />', () => {
-  it.skip('should render a div', () => {
-    const renderedComponent = shallow(
+  // let store;
+  let renderComponent;
+  // let renderConnectedComponent;
+
+  // beforeAll(() => {
+  //   store = configureStore({}, browserHistory);
+  // });
+
+  // Before each test reset the build data for safety
+  beforeEach(() => {
+    renderComponent = () => shallow(
       <Header />
-    );
-    expect(renderedComponent.find('div').length).toEqual(1);
+    ).dive();
+
+    // renderConnectedComponent = () => mount(
+    //   <Provider store={store}>
+    //     <LanguageProvider messages={translationMessages}>
+    //       <ConnectedHeader />
+    //     </LanguageProvider>
+    //   </Provider>
+    // );
+  });
+
+  it('should render a div', () => {
+    const renderedComponent = renderComponent();
+    expect(renderedComponent.type()).toEqual('div');
+  });
+
+  it('should render the site title', () => {
+    const renderedComponent = renderComponent();
+    expect(renderedComponent.find(Title).length).toEqual(1);
+    expect(renderedComponent.find(Title).dive().text()).toEqual('Factorio Builds');
+  });
+
+  it.skip('should render a navbar with login and create account', () => {
+    const renderedComponent = renderComponent();
+    expect(renderedComponent.find(HeaderLink).filterWhere(((n) => n.text() === 'Login')).length).toEqual(1);
+    expect(renderedComponent.find(HeaderLink).filterWhere(((n) => n.text() === 'Create Account')).length).toEqual(1);
   });
 });
